8f280e481bc69af098a5765c5ed529ed85263d03
DK_Peng <1063137960@qq.com>
2018-07-23 16:26:26 +0800

    Add : 增加路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,59 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import IndexPage from '@/components/index'
+import DuoMiIndexPage from '@/components/index'
+
+// import media files
+import DuoMiMediaRoutePage from '@/components/media/route'
+import DuoMiMediaMAreaPage from '@/components/media/area'
+import DuoMiMediaMoviePage from '@/components/media/movie'
+import DuoMiMediaMusicPage from '@/components/media/music'
+import DuoMiMediaStoryPage from '@/components/media/story'
+import DuoMiMediaDetailPage from '@/components/media/movie_detail'
+
 Vue.use(Router)
 
 export default new Router({
     routes : [
         {
-            path: '/',
-            name: 'home',
-            component: IndexPage,
+            path : '/',
+            name : 'index',
+            component : DuoMiIndexPage,
             meta : {
                 keepAlive : true
             }
         },
+        {
+            path : '/media',
+            name : 'media',
+            component : DuoMiMediaRoutePage,
+            redirect : '/media/m_area',
+            children : [
+                {
+                    path : 'm_area',
+                    name : 'm_area',
+                    component : DuoMiMediaMAreaPage,
+                },
+                {
+                    path : 'movie',
+                    name : 'movie',
+                    component : DuoMiMediaMoviePage,
+                },
+                {
+                    path : 'music',
+                    name : 'music',
+                    component : DuoMiMediaMusicPage,
+                },
+                {
+                    path : 'story',
+                    name : 'story',
+                    component : DuoMiMediaStoryPage,
+                },
+                {
+                    path : 'movie_detail/:m_id',
+                    name : 'detaile',
+                    component : DuoMiMediaDetailPage
+                }
+            ]
+        }
     ]
 })
